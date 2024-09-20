e8769596d9c7ec2d06274853e430cf03ca55932c
Edmund Lin <elz.work22@gmail.com>
2024-09-21 00:44:32 +0800

    test: link github and EC2

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body>
+	<header>
+		<h1>INF2006_CCBD</h1>
+	</header>
+	<main>
+
+	</main>
+	<footer>
+
+	</footer>
+</body>
+
+</html>
