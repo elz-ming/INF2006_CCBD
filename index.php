65918d8f5adc58f9fe9b0c705a8869f57da7efd1
Edmund Lin <elz.work22@gmail.com>
2024-09-21 01:18:53 +0800

    test: push 6

--- index.php
+++ index.php
@@ -13,7 +13,12 @@
 		<h1>INF2006_CCBD</h1>
 	</header>
 	<main>
-
+		<h2>Hi, I am Edmund</h2>
+		<h2>Hi, I am Rachel 1</h2>
+		<h2>Hi, I am Rachel 2</h2>
+		<h2>Hi, I am Kelisha</h2>
+		<h2>Hi, I am Han Yi (aka Rachel 3)</h2>
+		<h2>Hi, I am Manesh</h2>
 	</main>
 	<footer>
 
